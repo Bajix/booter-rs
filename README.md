58bfa1b46bd93a5db138aa4950e8266c51914771
Thomas Sieverding <Thomas@bajix.com>
2021-05-21 20:55:52 -0700

    Add booter::assert_booted as a means to ensure usage contract

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 [![Cargo](https://img.shields.io/crates/v/booter.svg)](https://crates.io/crates/booter)
 [![Documentation](https://docs.rs/booter/badge.svg)](https://docs.rs/booter)
 
-This crate allows a simple means to register and call one time initialization functions, the idea being this could be used in conjunction with [static_init](https://crates.io/crates/static_init) in order to create statics that can created pre-main and initalized post-main once Tokio is online and the enviroment configured.
+This crate allows a simple means to register and call one time initialization functions, the idea being this could be used in conjunction with [static_init](https://crates.io/crates/static_init) or std::mem::MaybeUninit in order to create statics that can be initalized once post-main after Tokio is online and the enviroment configured. 
 
 ```rust
 booter::call_on_boot!({
@@ -13,5 +13,6 @@ booter::call_on_boot!({
 
 fn main() {
   booter::boot();
+  booter::assert_booted();
 }
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,45 @@
+//! This crate allows a simple means to register and call one time initialization functions, the idea being this could be used in conjunction with [static_init](https://crates.io/crates/static_init) or std::mem::MaybeUninit in order to create statics that can be initalized once post-main after Tokio is online and the enviroment configured. 
+//! 
+//! ```rust
+//! booter::call_on_boot!({
+//!   println("Hello World!");
+//! });
+//! 
+//! fn main() {
+//!   booter::boot();
+//!   booter::assert_booted();
+//! }
+//! ```
+
+#[doc(hidden)]
 pub extern crate atomic_take;
+
+#[doc(hidden)]
 pub extern crate inventory;
 
 use atomic_take::AtomicTake;
+use std::sync::atomic::{AtomicBool, Ordering};
 
+#[doc(hidden)]
 pub struct BootBox {
   pub boot_fn: AtomicTake<Box<dyn FnOnce()>>,
 }
 
 inventory::collect!(BootBox);
 
-// Iterate over all boot functions as captured by booter::call_on_boot
+#[cfg(debug_assertions)]
+static BOOT_CALLED: AtomicBool = AtomicBool::new(false);
+
+/// Call all functions captured by booter::call_on_boot.
 pub fn boot() {
+  if cfg!(debug_assertions) {
+    assert_eq!(
+      BOOT_CALLED.compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed),
+      Ok(false),
+      "booter::boot should be called exactly once after env setup"
+    );
+  }
+
   for boot_box in inventory::iter::<BootBox> {
     if let Some(boot_fn) = boot_box.boot_fn.take() {
       boot_fn();
@@ -18,6 +47,18 @@ pub fn boot() {
   }
 }
 
+/// Development assertion to ensure booter::boot called exactly once. Release builds skip check
+pub fn assert_booted() {
+  if cfg!(debug_assertions) {
+    assert_eq!(
+      BOOT_CALLED.load(Ordering::Acquire),
+      true,
+      "booter::boot should be called exactly once after env setup"
+    );
+  }
+}
+
+/// Register FnOnce to be called on booter::boot
 #[macro_export]
 macro_rules! call_on_boot {
   ($boot_fn:block) => {
@@ -37,15 +78,22 @@ mod tests {
 
   use std::sync::atomic::{AtomicBool, Ordering};
 
-  static BOOT_CALLED: AtomicBool = AtomicBool::new(false);
+  static CALLBACK_CALLED: AtomicBool = AtomicBool::new(false);
 
   call_on_boot!({
-    BOOT_CALLED.store(true, Ordering::SeqCst);
+    CALLBACK_CALLED.store(true, Ordering::Release);
   });
+
+  #[test]
+  #[should_panic(expected = "booter::boot should be called exactly once after env setup")]
+  fn it_asserts_booter_booted() {
+    BOOT_CALLED.store(false, Ordering::Release);
+    assert_booted();
+  }
 
   #[test]
   fn it_boots() {
     boot();
-    assert!(BOOT_CALLED.load(Ordering::SeqCst));
+    assert!(CALLBACK_CALLED.load(Ordering::Acquire));
   }
 }
